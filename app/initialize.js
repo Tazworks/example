02b38daa98fbb8da1748fbcdf4806ebc37ef7f23
Mike Hoffert <mike.hoffert@gmail.com>
2017-10-04 10:31:13 -0600

    Firebase configuration

--- app/initialize.js
+++ app/initialize.js
@@ -1,8 +1,18 @@
 import Admin from "./pages/admin";
 import CostumeList from "./pages/costumeList";
+import Firebase from "firebase";
 import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+var config = {
+  apiKey: "taz-costumes",
+  authDomain: "taz-costumes.firebaseapp.com",
+  databaseURL: "https://taz-costumes.firebaseio.com",
+  storageBucket: "taz-costumes.appspot.com"
+};
+
+Firebase.initializeApp(config);
 
 class App extends React.Component {
   render() {
